98ce0e41723dfdbbd9568065471c2e2f6d677fd1
kazushifujiwara <kazushi@proteus-dt.com>
2025-11-14 11:21:49 +0800

    python

--- python/main.py
+++ python/main.py
@@ -11,3 +11,11 @@ def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
 
 # uvicorn main:app --reload
+# git clone <repo-url>
+# cd your-project
+
+# python3 -m venv venv
+# source venv/bin/activate  # Windows: venv\Scripts\activate
+
+# pip install -r requirements.txt
+# python main.py
